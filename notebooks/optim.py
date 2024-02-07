510f121cb69591c4410357f4703fe166cbaeab74
Dimitrije Markovic <5038100+dimarkov@users.noreply.github.com>
2024-02-07 17:17:34 +0100

    updated notebooks

--- notebooks/optim.py
+++ notebooks/optim.py
@@ -3,9 +3,8 @@ from typing import Any, NamedTuple, Optional, Union
 
 import chex
 import jax
-import jax.numpy as jnp
 
-from jax import random as jr
+from jax import random as jr, tree_util as jtu, numpy as jnp
 
 from optax._src import base
 from optax._src import combine
@@ -66,13 +65,102 @@ def update_second_moment(updates, moments, noise, decay, N, lam):
 
   return s
 
-class ScaleByState(NamedTuple):
+class ScaleByLieState(NamedTuple):
   """State for the algorithms."""
   key: chex.PRNGKey
-  count: chex.Array  # shape=(), dtype=jnp.int32.
-  m: base.Updates
-  s: base.Updates
+  N: int
+  noise: float
+  m_u: base.Updates
+  m_v: base.Updates
+  A: base.Updates
   ε: base.Updates
+
+
+# group-exponential
+def grpexp(rho, x, eps=1e-5):
+    return jax.lax.select(
+        jnp.abs(x) < eps, 
+        - rho + x * (rho ** 2.0) / 2, 
+        (jnp.exp(- rho * x) - 1) / x
+    )
+
+def scale_by_lie(
+    key: chex.PRNGKey,
+    lr: float,
+    init_scale: float,
+    temperature: float,
+    train_set_size: int,
+    num_samples: int,
+    init_noise: float = 1.0,
+    a1: float = 1.0,
+    a2: float = 1e-2,
+    b1: float = 0.9,
+    b2: float = 0.999,
+    c_X: float = 2.0,
+    c_y: float = 1.0,
+    m_dtype: Optional[chex.ArrayDType] = None
+) -> base.GradientTransformation:
+  """Rescale updates according to the Lie-Group Bayesian Learning Rule (BLR) algorithm.
+
+  References:
+    [EM Kıral et al, 2023](https://arxiv.org/abs/2303.04397)
+
+  Args:
+    scaled_prior_precision: prior precision on parameters divided by the number of data points.
+    scaled_initi_precision: initial precision for variational distribution q divided by the number of data points.
+    b1: Decay rate for the exponentially weighted average of grads.
+    b2: Decay rate for the exponentially weighted average of squared grads.
+    m_dtype: Optional `dtype` to be used for the first order accumulator; if
+      `None` then the `dtype` is inferred from `params` and `updates`.
+
+  Returns:
+    A `GradientTransformation` object.
+  """
+
+  m_dtype = utils.canonicalize_dtype(m_dtype)
+
+  s_init = init_scale
+  n_init = init_noise
+  N = train_set_size
+  S = num_samples
+  t_tau = temperature / N
+
+  def init_fn(key, params):
+    m_u = jtu.tree_map(lambda t: jnp.zeros_like(t, dtype=m_dtype), params)  # first moment
+    m_v = jtu.tree_map(lambda t: jnp.zeros_like(t, dtype=m_dtype), params) # second moment
+    A = jtu.tree_map(lambda t: s_init * jnp.ones_like(t, dtype=m_dtype), params)  # scale
+    
+    p_names = list(params.keys())
+    key, _key = jr.split(key)
+    keys = {k:v for k, v in zip(p_names, list(jr.split(_key, len(p_names))))}
+    ε = jtu.tree_map(lambda key, t: n_init * jr.normal(key, shape=(S, *t.shape), dtype=m_dtype), keys, params)  # noise
+    return ScaleByLieState(key=key, m_u=m_u, m_v=m_v, A=A, ε=ε, N=N, noise=n_init)
+
+  def update_fn(updates, state, params):
+
+    key, _key = jr.split(state.key)
+
+    V_k = jtu.tree_map(lambda s, g: s * g, state.A, updates)
+    V = jtu.tree_map(lambda v: v.mean(0), V_k)
+    U = jtu.tree_map(lambda v, e: jnp.mean(e * v, 0), V_k, state.ε)
+
+    m_u = jtu.tree_map(lambda m, u: b2 * m + (1 - b2) * (u - t_tau), state.m_u, U)
+    m_v = jtu.tree_map(lambda m, v: b1 * m + (1 - b1) * v, state.m_v, V)
+
+    A = jtu.tree_map(lambda a, m: a * jnp.exp(- lr * a2 * m), state.A, m_u)
+    updates = jtu.tree_map(
+        lambda a, m1, m2: c_X * a * m2 * grpexp(lr * a1, m1) / c_y, state.A, m_u, m_v
+      )
+    
+    (m_u, m_v, A) = utils.cast_tree((m_u, m_v, A), m_dtype)
+
+    p_names = list(params.keys())
+    keys = {k:v for k, v in zip(p_names, list(jr.split(_key, len(p_names))))}
+    ε = jtu.tree_map(lambda key, t: n_init * jr.normal(key, shape=(S, *t.shape), dtype=m_dtype), keys, params)  # noise
+
+    return updates, ScaleByLieState(key=key, m_u=m_u, m_v=m_v, A=A, ε=ε, N=N, noise=n_init)
+
+  return base.GradientTransformation(functools.partial(init_fn, key), update_fn)
 
 
 def scale_by_von(
@@ -299,6 +387,40 @@ def _scale_by_learning_rate(learning_rate: ScalarOrSchedule, flip_sign=True):
   if callable(learning_rate):
     return transform.scale_by_schedule(lambda count: m * learning_rate(count))
   return transform.scale(m * learning_rate)
+  
+def lieblr(
+    key: chex.PRNGKey,
+    learning_rate: ScalarOrSchedule,
+    init_scale: float = 1.0,
+    init_noise: float = 1e-2,
+    temperature: float = 1.0,
+    train_set_size: int = 1,
+    num_samples: int = 1,
+    a1: float = 1.0,
+    a2: float = 1e-2,
+    b1: float = 0.9,
+    b2: float = 0.999,
+    c_X: float = 2.0,
+    c_y: float = 1.0,
+    m_dtype: Optional[Any] = None
+) -> base.GradientTransformation:
+  lr = learning_rate #_scale_by_learning_rate(learning_rate, flip_sign=False)
+  return scale_by_lie(
+    key,
+    lr,
+    init_scale,
+    temperature,
+    train_set_size,
+    num_samples,
+    init_noise=init_noise,
+    a1=a1,
+    a2=a2,
+    b1=b1,
+    b2=b2,
+    c_X=c_X,
+    c_y=c_y,
+    m_dtype=m_dtype
+    )
 
 def vadam(
     key: chex.PRNGKey,
