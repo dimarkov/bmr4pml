9482f793930b12f941041596ab6dcbb763535917
Dimitrije Markovic <5038100+dimarkov@users.noreply.github.com>
2024-02-09 16:26:03 +0100

    testing of Lie-BLR with BMR on MLP and LeNet

--- notebooks/optim.py
+++ notebooks/optim.py
@@ -77,7 +77,7 @@ class ScaleByLieState(NamedTuple):
 
 
 # group-exponential
-def grpexp(rho, x, eps=1e-5):
+def grpexp(rho, x, eps=1e-6):
     return jax.lax.select(
         jnp.abs(x) < eps, 
         - rho + x * (rho ** 2.0) / 2, 
@@ -93,7 +93,7 @@ def scale_by_lie(
     num_samples: int,
     init_noise: float = 1.0,
     a1: float = 1.0,
-    a2: float = 1e-2,
+    a2: float = 1.0,
     b1: float = 0.9,
     b2: float = 0.999,
     c_X: float = 2.0,
@@ -133,30 +133,30 @@ def scale_by_lie(
     p_names = list(params.keys())
     key, _key = jr.split(key)
     keys = {k:v for k, v in zip(p_names, list(jr.split(_key, len(p_names))))}
-    ε = jtu.tree_map(lambda key, t: n_init * jr.normal(key, shape=(S, *t.shape), dtype=m_dtype), keys, params)  # noise
+    ε = jtu.tree_map(lambda t, key: n_init * jr.normal(key, shape=(S, *t.shape), dtype=m_dtype), params, keys)  # noise
     return ScaleByLieState(key=key, m_u=m_u, m_v=m_v, A=A, ε=ε, N=N, noise=n_init)
 
-  def update_fn(updates, state, params):
+  def update_fn(grads, state, params):
 
     key, _key = jr.split(state.key)
 
-    V_k = jtu.tree_map(lambda s, g: s * g, state.A, updates)
+    V_k = jtu.tree_map(lambda s, g: s * g, state.A, grads)
     V = jtu.tree_map(lambda v: v.mean(0), V_k)
     U = jtu.tree_map(lambda v, e: jnp.mean(e * v, 0), V_k, state.ε)
 
-    m_u = jtu.tree_map(lambda m, u: b2 * m + (1 - b2) * (u - t_tau), state.m_u, U)
+    m_u = jtu.tree_map(lambda m, u: b2 * m + (1 - b2) * u, state.m_u, U)
     m_v = jtu.tree_map(lambda m, v: b1 * m + (1 - b1) * v, state.m_v, V)
 
-    A = jtu.tree_map(lambda a, m: a * jnp.exp(- lr * a2 * m), state.A, m_u)
+    A = jtu.tree_map(lambda a, m: a * jnp.exp(- lr * a2 * (m - t_tau)), state.A, m_u)
     updates = jtu.tree_map(
-        lambda a, m1, m2: c_X * a * m2 * grpexp(lr * a1, m1) / c_y, state.A, m_u, m_v
+        lambda a, m1, m2: c_X * a * m2 * grpexp(lr * a1, (m1 - t_tau)) / c_y, state.A, m_u, m_v
       )
     
     (m_u, m_v, A) = utils.cast_tree((m_u, m_v, A), m_dtype)
 
     p_names = list(params.keys())
     keys = {k:v for k, v in zip(p_names, list(jr.split(_key, len(p_names))))}
-    ε = jtu.tree_map(lambda key, t: n_init * jr.normal(key, shape=(S, *t.shape), dtype=m_dtype), keys, params)  # noise
+    ε = jtu.tree_map(lambda t, key: n_init * jr.normal(key, shape=(S, *t.shape), dtype=m_dtype), params, keys)  # noise
 
     return updates, ScaleByLieState(key=key, m_u=m_u, m_v=m_v, A=A, ε=ε, N=N, noise=n_init)
 
@@ -217,171 +217,6 @@ def scale_by_von(
 
   return base.GradientTransformation(init_fn, update_fn)
 
-def scale_by_vadam(
-    key: chex.PRNGKey,
-    scaled_prior_precision: float,
-    scaled_init_precision: float,
-    train_set_size: int,
-    b1: float = 0.9,
-    b2: float = 0.999,
-    eps_root: float = 0.0,
-    m_dtype: Optional[chex.ArrayDType] = None
-) -> base.GradientTransformation:
-  """Rescale updates according to the Adam algorithm.
-
-  References:
-    [Kingma et al, 2014](https://arxiv.org/abs/1412.6980)
-
-  Args:
-    scaled_prior_precision: prior precision on parameters divided by the number of data points.
-    scaled_initi_precision: initial precision for variational distribution q divided by the number of data points.
-    b1: Decay rate for the exponentially weighted average of grads.
-    b2: Decay rate for the exponentially weighted average of squared grads.
-    eps_root: Term added to the denominator inside the square-root to improve
-      numerical stability when backpropagating gradients through the rescaling.
-    m_dtype: Optional `dtype` to be used for the first order accumulator; if
-      `None` then the `dtype` is inferred from `params` and `updates`.
-
-  Returns:
-    A `GradientTransformation` object.
-  """
-
-  m_dtype = utils.canonicalize_dtype(m_dtype)
-
-  s_init = max(1e-16, scaled_init_precision - scaled_prior_precision)
-  scale_init = jnp.sqrt( 1 / (train_set_size * scaled_init_precision) )
-  t_lam = scaled_prior_precision
-
-  def init_fn(params):
-    mus, aux = jax.tree_util.tree_flatten(params)
-    m = []
-    s = []
-    ε = []
-    keys = jr.split(key, len(mus) + 1)
-    for p, _key in zip(mus, keys[:-1]):
-      m.append( jnp.zeros_like(p, dtype=m_dtype) )
-      s.append( s_init * jnp.ones_like(p) )
-      ε.append( scale_init * jr.normal(_key, shape=p.shape) )
-
-    m = jax.tree_util.tree_unflatten(aux, m)  # First moment
-    s = jax.tree_util.tree_unflatten(aux, s)  # Second moment
-    ε = jax.tree_util.tree_unflatten(aux, ε)  # Noise
-    return ScaleByState(key=keys[-1], count=jnp.zeros([], jnp.int32), m=m, s=s, ε=ε)
-
-  def update_fn(updates, state, params):
-    m = update_moment(updates, state.m, params, b1, t_lam)
-    s = update_moment_per_elem_norm(updates, state.s, b2, 2)
-    count_inc = numerics.safe_int32_increment(state.count)
-    m_hat = bias_correction(m, b1, count_inc)
-    s_hat = bias_correction(s, b2, count_inc)
-
-    m_hat_flat, aux = jax.tree_util.tree_flatten(m_hat)
-    s_hat_flat, _ = jax.tree_util.tree_flatten(s_hat)
-    s_flat, _ = jax.tree_util.tree_flatten(s)
-
-    key = state.key
-    updates = []
-    ε = []
-    for x, y, π, _key in zip(m_hat_flat, s_hat_flat, s_flat, jr.split(key, len(m_hat_flat))):
-      updates.append( x / (jnp.sqrt(y + eps_root) + t_lam) )
-      scale = jnp.sqrt( 1 / (train_set_size * (π + t_lam)) )
-      ε.append( scale * jr.normal(_key, shape=scale.shape))
-    
-    updates = jax.tree_util.tree_unflatten(aux, updates)
-    ε = jax.tree_util.tree_unflatten(aux, ε)
-    m = utils.cast_tree(m, m_dtype)
-    key, _ = jr.split(_key)
-
-    return updates, ScaleByState(key=key, count=count_inc, m=m, s=s, ε=ε)
-
-  return base.GradientTransformation(init_fn, update_fn)
-
-
-def scale_by_vadabelief(
-    key: chex.PRNGKey,
-    scaled_prior_precision: float,
-    scaled_init_precision: float,
-    train_set_size: int,
-    b1: float = 0.9,
-    b2: float = 0.999,
-    eps_root: float = 1e-16,
-    m_dtype: Optional[chex.ArrayDType] = None,
-) -> base.GradientTransformation:
-  """Rescale updates according to the variational AdaBelief algorithm.
-
-  Args:
-    scaled_prior_precision: prior precision on parameters divided by the number of data points.
-    scaled_initi_precision: initial precision for variational distribution q divided by the number of data points.
-    b1: Decay rate for the exponentially weighted average of grads.
-    b2: Decay rate for the exponentially weighted average of squared grads.
-    eps_root: Term added to the denominator inside the square-root to improve
-      numerical stability when backpropagating gradients through the rescaling.
-    m_dtype: Optional `dtype` to be used for the first order accumulator; if
-      `None` then the `dtype` is inferred from `params` and `updates`.
-
-  Returns:
-    A `GradientTransformation` object.
-  """
-
-  m_dtype = utils.canonicalize_dtype(m_dtype)
-
-  s_init = max(0., scaled_init_precision - scaled_prior_precision)
-  scale_init = jnp.sqrt( 1 / (train_set_size * scaled_init_precision) )
-  t_lam = scaled_prior_precision
-
-  def init_fn(params):
-    m = jax.tree_util.tree_map(  # First moment
-        lambda t: jnp.zeros_like(t, dtype=m_dtype), params)
-    s = jax.tree_util.tree_map(lambda x: s_init * jnp.ones_like(x), params)  # Second moment
-    return ScaleByState(count=jnp.zeros([], jnp.int32), m=m, s=s)
-  
-  def init_fn(params):
-    mus, aux = jax.tree_util.tree_flatten(params)
-    m = []
-    s = []
-    ε = []
-    keys = jr.split(key, len(mus) + 1)
-    for p, _key in zip(mus, keys[:-1]):
-      m.append( jnp.zeros_like(p, dtype=m_dtype) )
-      s.append( s_init * jnp.ones_like(p) )
-      ε.append( scale_init * jr.normal(_key, shape=p.shape) )
-
-    m = jax.tree_util.tree_unflatten(aux, m)  # First moment
-    s = jax.tree_util.tree_unflatten(aux, s)  # Second moment
-    ε = jax.tree_util.tree_unflatten(aux, ε)  # Noise
-    return ScaleByState(key=keys[-1], count=jnp.zeros([], jnp.int32), m=m, s=s, ε=ε)
-
-  def update_fn(updates, state, params):
-    m = update_moment(updates, state.m, params, b1, t_lam)
-    prediction_error = jax.tree_util.tree_map(lambda g, m: g - m, updates, state.m)
-    s = update_moment_per_elem_norm(prediction_error, state.s, b2, 2)
-    s = jax.tree_util.tree_map(lambda v: v + eps_root, s)
-    count_inc = numerics.safe_int32_increment(state.count)
-    m_hat = bias_correction(m, b1, count_inc)
-    s_hat = bias_correction(s, b2, count_inc)
-
-    m_hat_flat, aux = jax.tree_util.tree_flatten(m_hat)
-    s_hat_flat, _ = jax.tree_util.tree_flatten(s_hat)
-    s_flat, _ = jax.tree_util.tree_flatten(s)
-
-    key = state.key
-    updates = []
-    ε = []
-    for x, y, π, _key in zip(m_hat_flat, s_hat_flat, s_flat, jr.split(key, len(m_hat_flat))):
-      updates.append( x / (jnp.sqrt(y + eps_root) + t_lam) )
-      scale = jnp.sqrt( 1 / (train_set_size * (π + t_lam)) )
-      ε.append( scale * jr.normal(_key, shape=scale.shape))
-    
-    updates = jax.tree_util.tree_unflatten(aux, updates)
-    ε = jax.tree_util.tree_unflatten(aux, ε)
-    m = utils.cast_tree(m, m_dtype)
-    key, _ = jr.split(_key)
-
-    return updates, ScaleByState(key=key, count=count_inc, m=m, s=s, ε=ε)
-
-  return base.GradientTransformation(init_fn, update_fn)
-
-
 def _scale_by_learning_rate(learning_rate: ScalarOrSchedule, flip_sign=True):
   m = -1 if flip_sign else 1
   if callable(learning_rate):
@@ -392,12 +227,12 @@ def lieblr(
     key: chex.PRNGKey,
     learning_rate: ScalarOrSchedule,
     init_scale: float = 1.0,
-    init_noise: float = 1e-2,
+    init_noise: float = 1.0,
     temperature: float = 1.0,
     train_set_size: int = 1,
     num_samples: int = 1,
     a1: float = 1.0,
-    a2: float = 1e-2,
+    a2: float = 1.0,
     b1: float = 0.9,
     b2: float = 0.999,
     c_X: float = 2.0,
@@ -421,137 +256,3 @@ def lieblr(
     c_y=c_y,
     m_dtype=m_dtype
     )
-
-def vadam(
-    key: chex.PRNGKey,
-    learning_rate: ScalarOrSchedule,
-    t_lam: float,
-    t_init: float,
-    N: int, 
-    b1: float = 0.9,
-    b2: float = 0.999,
-    eps_root: float = 0.0,
-    m_dtype: Optional[Any] = None,
-) -> base.GradientTransformation:
-  r"""The variational Adam (Vadam) optimizer.
-
-  Vadam is a stochastic natural gradient variant with gradient scaling adaptation. The scaling
-  used for each parameter is computed from estimates of first and second-order
-  moments of the gradients (using suitable exponential moving averages).
-
-  Let :math:`\alpha_t` represent the learning rate and :math:`\beta_1, \beta_2`,
-  :math:`\varepsilon`, :math:`\bar{\varepsilon}` represent the arguments
-  ``b1``, ``b2``, ``eps`` and ``eps_root`` respectievly. The learning rate is
-  indexed by :math:`t` since the learning rate may also be provided by a
-  schedule function.
-
-  The ``init`` function of this optimizer initializes an internal state
-  :math:`S_0 := (m_0, s_0) = (0, (init_precision - prior_precision ) / train_set_size)`, representing initial estimates for the first and second moments. In practice these values are stored as pytrees
-  , with the same shape as the model updates.
-  At step :math:`t`, the ``update`` function of this optimizer takes as
-  arguments the incoming gradients :math:`g_t` and optimizer state :math:`S_t`
-  and computes updates :math:`u_t` and new state :math:`S_{t+1}`. Thus, for
-  :math:`t > 0`, we have,
-
-  .. math::
-    \begin{align*}
-      m_t &\leftarrow \beta_1 \cdot m_{t-1} + (1-\beta_1) \cdot ( g_t + \tilde{\lambda} \mu ) \\
-      s_t &\leftarrow \beta_2 \cdot s_{t-1} + (1-\beta_2) \cdot {g_t}^2 \\
-      \hat{m}_t &\leftarrow m_t / {(1-\beta_1^t)} \\
-      \hat{s}_t &\leftarrow s_t / {(1-\beta_2^t)} \\
-      u_t &\leftarrow \alpha_t \cdot \hat{m}_t / \left({\sqrt{\hat{v}_t +
-      \bar{\varepsilon}} + \tilde{\lambda}} \right)\\
-      S_t &\leftarrow (m_t, s_t).
-    \end{align*}
-
-  References:
-    Khan et al, 2018: https://arxiv.org/abs/1806.04854
-
-  Args:
-    key: Random number seed.
-    learning_rate: A fixed global scaling factor.
-    t_lam: Prior precision devided by the number of data points in the training set.
-    t_init: Initial precision of the variational posterior q devided by the number of data points in the training set.
-    N: Number of data points in the training set.
-    b1: Exponential decay rate to track the first moment of past gradients.
-    b2: Exponential decay rate to track the second moment of past gradients.
-    eps_root: A small constant applied to denominator inside the square root (as
-      in RMSProp), to avoid dividing by zero when rescaling. This is needed for
-      example when computing (meta-)gradients through Vadam.
-    m_dtype: Optional `dtype` to be used for the first order accumulator; if
-      `None` then the `dtype` is inferred from `params` and `updates`.
-
-  Returns:
-    The corresponding `GradientTransformation`.
-  """
-  return combine.chain(
-      scale_by_vadam(key, t_lam, t_init, N, b1=b1, b2=b2, eps_root=eps_root, m_dtype=m_dtype),
-      _scale_by_learning_rate(learning_rate),
-  )
-
-def vadabelief(
-    key: chex.PRNGKey,
-    learning_rate: ScalarOrSchedule,
-    t_lam: float,
-    t_init: float,
-    N: int, 
-    b1: float = 0.9,
-    b2: float = 0.999,
-    eps_root: float = 1e-16,
-    m_dtype: Optional[Any] = None,
-) -> base.GradientTransformation:
-  r"""The variational Adabelief (Vadabelief) optimizer.
-
-  Vadabelief is a stochastic natural gradient variant with gradient scaling adaptation. The scaling
-  used for each parameter is computed from estimates of first and second-order
-  moments of the gradients (using suitable exponential moving averages).
-
-  Let :math:`\alpha_t` represent the learning rate and :math:`\beta_1, \beta_2`,
-  :math:`\varepsilon`, :math:`\bar{\varepsilon}` represent the arguments
-  ``b1``, ``b2``, ``eps`` and ``eps_root`` respectievly. The learning rate is
-  indexed by :math:`t` since the learning rate may also be provided by a
-  schedule function.
-
-  The ``init`` function of this optimizer initializes an internal state
-  :math:`S_0 := (m_0, s_0) = (0, (init_precision - prior_precision ) / train_set_size)`, representing initial estimates for the first and second moments. In practice these values are stored as pytrees
-  , with the same shape as the model updates.
-  At step :math:`t`, the ``update`` function of this optimizer takes as
-  arguments the incoming gradients :math:`g_t` and optimizer state :math:`S_t`
-  and computes updates :math:`u_t` and new state :math:`S_{t+1}`. Thus, for
-  :math:`t > 0`, we have,
-
-  .. math::
-    \begin{align*}
-      m_t &\leftarrow \beta_1 \cdot m_{t-1} + (1-\beta_1) \cdot ( g_t + \tilde{\lambda} \mu ) \\
-      s_t &\leftarrow \beta_2 \cdot s_{t-1} + (1-\beta_2) \cdot {g_t}^2 \\
-      \hat{m}_t &\leftarrow m_t / {(1-\beta_1^t)} \\
-      \hat{s}_t &\leftarrow s_t / {(1-\beta_2^t)} \\
-      u_t &\leftarrow \alpha_t \cdot \hat{m}_t / \left({\sqrt{\hat{v}_t +
-      \bar{\varepsilon}} + \tilde{\lambda}} \right)\\
-      S_t &\leftarrow (m_t, s_t).
-    \end{align*}
-
-  References:
-    Khan et al, 2018: https://arxiv.org/abs/1806.04854
-
-  Args:
-    learning_rate: A fixed global scaling factor.
-    t_lam: Prior precision devided by the number of data points in the training set.
-    t_init: Initial precision of the variational posterior q devided by the number of data points in the training set.
-    b1: Exponential decay rate to track the first moment of past gradients.
-    b2: Exponential decay rate to track the second moment of past gradients.
-    eps_root: A small constant applied to denominator inside the square root (as
-      in RMSProp), to avoid dividing by zero when rescaling. This is needed for
-      example when computing (meta-)gradients through Vadam.
-    m_dtype: Optional `dtype` to be used for the first order accumulator; if
-      `None` then the `dtype` is inferred from `params` and `updates`.
-
-  Returns:
-    The corresponding `GradientTransformation`.
-  """
-  return combine.chain(
-      scale_by_vadabelief(
-          key, t_lam, t_init, N, b1=b1, b2=b2, eps_root=eps_root, m_dtype=m_dtype
-        ),
-      _scale_by_learning_rate(learning_rate),
-  )
